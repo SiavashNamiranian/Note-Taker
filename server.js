beca415493a26a6589e8d335fe0f1d72a6db65ca
Accurast <siavash65n@gmail.com>
2023-06-18 15:56:35 -0700

    updates

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require('express');
 const data = require('./Develop/db/db.json')
 
 // Import built-in Node.js package 'path' to resolve path of files that are located on the server
-const path = require('path');
+// const path = require('path');
 
 
-const api = require('./Develop/public/assets/js/index.js');
+// const api = require('./Develop/public/assets/js/index.js');
 
 // Initialize an instance of Express.js
 const app = express();
