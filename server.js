150fd7776656cd414a8fdc20277af3261e9b7967
Accurast <siavash65n@gmail.com>
2023-06-19 10:34:17 -0700

    changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const api = require('./Develop/public/assets/js/index');
+
 
 
 const PORT = process.env.PORT || 3002;
@@ -12,13 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('Develop/public'));
 
-app.use('/api', api);
+
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+);
+
+app.get('/api', (req, res) =>
   res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
 );
 
